d1c0a928c71c2c50009ed701d9803b5729ed1053
Bob Belderbos <bob@pybit.es>
2018-07-29 01:12:48 +0200

    whitenoise

--- myreadinglist/wsgi.py
+++ myreadinglist/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for myreadinglist project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myreadinglist.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
