31006b3afe32be4fc78e9a4f4af17b0220d62868
Bob Belderbos <bob@pybit.es>
2018-07-29 02:44:32 +0200

    fix env variables

--- myreadinglist/settings.py
+++ myreadinglist/settings.py
@@ -135,11 +135,11 @@ STATICFILES_DIRS = (
 # https://github.com/pybites/django-registration/blob/master/register/settings.py
 ACCOUNT_ACTIVATION_DAYS = 7
 
-EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_USER = config('SENDGRID_USERNAME')
 EMAIL_HOST= 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_HOST_PASSWORD = config('SENDGRID_PASSWORD')
 
 LOGGING = {
     'version': 1,
