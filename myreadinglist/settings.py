3ec15f0fd233f7eab2d51b4bdcd687f0f429594f
Bob Belderbos <bobbelderbos@gmail.com>
2018-09-22 11:37:06 +0200

    undo last change pin registration to 2.0

--- myreadinglist/settings.py
+++ myreadinglist/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'django_registration',
     'myreadinglist',
     'books',
 ]
