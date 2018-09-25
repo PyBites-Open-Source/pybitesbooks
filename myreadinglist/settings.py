cad3e56414b7610a8afb7a39d21f904af18417ab
Bob Belderbos <bobbelderbos@gmail.com>
2018-09-25 12:40:09 +0200

    ValueError: Missing staticfiles manifest entry for 'favicon.ico'

--- myreadinglist/settings.py
+++ myreadinglist/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
