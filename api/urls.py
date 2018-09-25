90cbf81a2f5112ae7f9a31cf4f0e51798a95cfe5
Bob Belderbos <bobbelderbos@gmail.com>
2018-09-25 11:46:25 +0200

    random book endpoint

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^get_books/', main_views.get_books, name='get_books'),
     url(r'^users/$', views.user_books, name='user_books'),
     url(r'^users/(?P<username>.*)$', views.user_books, name='user_books'),
+    url(r'^random/$', views.random_book, name='random_book'),
+    url(r'^random/(?P<grep>.*)$', views.random_book, name='random_book'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
 from collections import defaultdict, Counter
 import json
+from random import randint, choice
 
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
-from books.models import UserBook
+from books.models import Book, UserBook
 
 
 def user_books(request, username=None):
@@ -23,9 +24,34 @@ def user_books(request, username=None):
 
         for book in books:
             data[book.status].append(dict(bookid=book.book.bookid,
-                                        title=book.book.title,
-                                        authors=book.book.authors,
-                                        completed=book.completed))
+                                          title=book.book.title,
+                                          authors=book.book.authors,
+                                          completed=book.completed))
+
+    json_data = json.dumps(data, indent=4, default=str, sort_keys=False)
+
+    return HttpResponse(json_data, content_type='application/json')
+
+
+def random_book(request, grep=None):
+    """Return a random book with optional filter"""
+    books = Book.objects.all()
+
+    if grep is not None:
+        books = books.filter(title__icontains=grep.lower())
+        book = choice(books)
+    else:
+        count = books.count()
+        book = books[randint(0, count - 1)]
+
+    data = dict(bookid=book.bookid,
+                title=book.title,
+                authors=book.authors,
+                published=book.published,
+                isbn=book.isbn,
+                pages=book.pages,
+                language=book.language,
+                description=book.description)
 
     json_data = json.dumps(data, indent=4, default=str, sort_keys=False)
 
