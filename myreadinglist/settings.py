05f1cf62fb7a2bae251d47bf665d0bce9053e0e3
Bob Belderbos <bobbelderbos@gmail.com>
2018-09-25 12:43:46 +0200

    update middleware

--- myreadinglist/settings.py
+++ myreadinglist/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
