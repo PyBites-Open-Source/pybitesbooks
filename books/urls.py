395f57d9fc4e61b2ae43b7a44c9d879dd497901b
Bob Belderbos <bobbelderbos@gmail.com>
2020-10-20 20:10:45 +0200

    use new syntax for urls

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 
 from books import views as book_views
 
 app_name = 'books'
 urlpatterns = [
-    url(r'^(?P<bookid>.*)$', book_views.book_page, name='book_page'),
+    path('<str:bookid>', book_views.book_page, name='book_page'),
 ]
