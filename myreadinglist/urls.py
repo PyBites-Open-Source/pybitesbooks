cd0f003d2ca22244c164d2ab379ffbdb3c4e1986
Bob Belderbos <bobbelderbos@gmail.com>
2020-10-24 20:55:00 +0200

    fix for https://github.com/PyBites-Open-Source/pbreadinglist/issues/22

--- myreadinglist/urls.py
+++ myreadinglist/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django_registration.backends.activation.views import RegistrationView
+from django_registration.forms import RegistrationFormUniqueEmail
 
 from . import views
 from books import views as book_views
@@ -13,7 +15,12 @@ urlpatterns = [
     path('books/', include('books.urls', namespace='books')),
     path('users/favorite/', book_views.user_favorite, name='favorite'),
     path('users/<str:username>', book_views.user_page, name='user_page'),
-    path('widget/<str:username>', book_views.user_page_widget, name='user_page_widget'),
+    path('widget/<str:username>', book_views.user_page_widget,
+         name='user_page_widget'),
+    # no dup emails - https://stackoverflow.com/a/19383392
+    path('accounts/register/',
+         RegistrationView.as_view(form_class=RegistrationFormUniqueEmail),
+         name='registration_register'),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('5hours/', include('pomodoro.urls')),
