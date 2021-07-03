1acee4a4454179c9957cfc7f0469d1db39079866
Bob Belderbos <bobbelderbos@gmail.com>
2021-07-03 19:08:52 +0200

    don't allow a user to add the same list twice

--- lists/views.py
+++ lists/views.py
@@ -28,6 +28,10 @@ class UserListCreateView(CreateView):
 
     def form_valid(self, form):
         form.instance.name = slugify(form.instance.name)
+        new_value = form.cleaned_data['name']
+        if UserList.objects.filter(name=new_value).count() > 0:
+            form.add_error('name', 'You already have this list')
+            return self.form_invalid(form)
         form.instance.user = self.request.user
         return super().form_valid(form)
 
@@ -39,6 +43,13 @@ class UserListUpdateView(OwnerRequiredMixin, UpdateView):
 
     def form_valid(self, form):
         form.instance.name = slugify(form.instance.name)
+        old_value = UserList.objects.get(pk=self.object.pk).name
+        new_value = form.cleaned_data['name']
+        # this if is there in case user saves existing value without change
+        if old_value != new_value:
+            if UserList.objects.filter(name=new_value).count() > 0:
+                form.add_error('name', 'You already have this list')
+                return self.form_invalid(form)
         return super().form_valid(form)
 
 
