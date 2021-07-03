efd71946640b36280c249f3eb8c1a3b37ff61c4f
Bob Belderbos <bobbelderbos@gmail.com>
2021-07-03 13:50:09 +0200

    add user in url pattern in case 2 users make the same list

--- lists/urls.py
+++ lists/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.UserListListView.as_view(), name='lists-view'),
-    path('<str:name>', views.UserListDetailView.as_view(), name='lists-detail'),
+    path('<str:name>/<str:username>', views.UserListDetailView.as_view(), name='lists-detail'),
     path('add/', views.UserListCreateView.as_view(), name='lists-add'),
     path('<int:pk>/', views.UserListUpdateView.as_view(), name='lists-update'),
     path('<int:pk>/delete/', views.UserListDeleteView.as_view(), name='lists-delete'),
