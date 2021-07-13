cb44488a2921b1d6287bf21d4b861b31b9cfbc5d
Bob Belderbos <bobbelderbos@gmail.com>
2021-07-13 15:48:02 +0200

    explicitly set test db for tests

--- myreadinglist/test_settings.py
+++ myreadinglist/test_settings.py
@@ -1,3 +1,13 @@
+from decouple import config
+import dj_database_url
+
 from myreadinglist.settings import *  # noqa F403
 
 STATICFILES_STORAGE = ''
+TEST_DB = config('TEST_DATABASE_URL',
+                 'sqlite:///test.db')
+DATABASES = {
+    'default': dj_database_url.config(
+        default=TEST_DB
+    )
+}
