06fa868470eec92e2ccabd68606b53f0acdaa444
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-04 16:56:37 +0200

    improve admin interface

--- books/admin.py
+++ books/admin.py
@@ -1,31 +1,63 @@
+from django.conf import settings
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import (Book, Search, UserBook,
-                     BookNote, Badge, BookConversion)
+                     BookNote, Badge, BookConversion,
+                     ImportedBook)
 
 
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("bookid", "title", "authors", "publisher", "pages", "inserted")
+    search_fields = ("title", "authors", "publisher")
 
 
 class SearchAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("term", "user", "inserted")
+    search_fields = ("term", "user__username")
 
 
 class UserBookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("user", "book", "status", "favorite", "completed", "inserted")
+    search_fields = ("user__username", "book__title",)
+    list_filter = ("status", "favorite")
 
 
 class BookNoteAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("user", "book", "type_note", "short_desc", "public", "inserted")
+    search_fields = ("description", "user__username")
+    list_filter = ("public", "type_note")
+
+    def short_desc(self, obj):
+        limit = 30
+        desc = obj.description if obj.description else ""
+        if len(desc) > limit:
+            return f"{obj.description[:limit]} ..."
+        else:
+            return obj.description
+    short_desc.short_description = 'short description'
 
 
 class BadgeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("books", "title")
 
 
 class BookConversionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("goodreads_id", "book_link", "inserted")
+    search_fields = ("goodreads_id", "googlebooks_id")
+
+    def book_link(self, obj):
+        return mark_safe(
+            f"<a href='{settings.DOMAIN}/books/{obj.googlebooks_id}' "
+            f"target='_blank'>{obj.googlebooks_id}</a>"
+        )
+    book_link.short_description = 'Google / PyBites book link'
+
+
+class ImportedBookAdmin(admin.ModelAdmin):
+    list_display = ("title", "book", "reading_status", "date_completed",
+                    "book_status", "user")
+    search_fields = ("title",)
 
 
 admin.site.register(Book, BookAdmin)
@@ -34,3 +66,4 @@ admin.site.register(UserBook, UserBookAdmin)
 admin.site.register(BookNote, BookNoteAdmin)
 admin.site.register(Badge, BadgeAdmin)
 admin.site.register(BookConversion, BookConversionAdmin)
+admin.site.register(ImportedBook, ImportedBookAdmin)
