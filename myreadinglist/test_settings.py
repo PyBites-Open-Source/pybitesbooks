61930d6d1732ffb9d2da7ad67ce72da4a62e77ba
Bob Belderbos <bobbelderbos@gmail.com>
2023-06-06 19:25:22 +0200

    fixes https://github.com/PyBites-Open-Source/pybitesbooks/issues/76

--- myreadinglist/test_settings.py
+++ myreadinglist/test_settings.py
@@ -4,10 +4,4 @@ import dj_database_url
 from myreadinglist.settings import *  # noqa F403
 
 STATICFILES_STORAGE = ''
-TEST_DB = config('TEST_DATABASE_URL',
-                 'sqlite:///test.db')
-DATABASES = {
-    'default': dj_database_url.config(
-        default=TEST_DB
-    )
-}
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
