99455fd3712d47e71f550c6fd10ae843fe4692b0
Bob Belderbos <bobbelderbos@gmail.com>
2023-06-08 11:49:12 +0200

    pass pomo_minutes to the template

--- pomodoro/views.py
+++ pomodoro/views.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 
 from pomodoro.models import (Pomodoro,
                              DEFAULT_POMO_GOAL,
+                             DEFAULT_POMO_MIN,
                              this_week)
 
 
@@ -27,5 +28,6 @@ def track_pomodoro(request):
     context = {
         'week_stats': sorted(week_stats.items(), reverse=True),
         'week_goal': DEFAULT_POMO_GOAL,
+        'pomo_minutes': DEFAULT_POMO_MIN,
     }
     return render(request, 'pomodoro/pomodoro.html', context)
